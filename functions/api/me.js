f2ca94f19bdebe124f506cdc6205e9bf18fa1dcf
Ruper02 <albertodelcastillo_95@hotmail.com>
2026-09-11 11:37:50 +0200

    Delete functions directory

--- functions/api/me.js
+++
@@ -1,8 +0,0 @@
-import { getUser } from "../_lib/auth.js";
-import { fail, json } from "../_lib/http.js";
-
-export async function onRequestGet({ request, env }) {
-  const user = await getUser(request, env);
-  if (!user) return fail("Acceso no autorizado. Inicia sesión con tu cuenta @iberia.es.", 401);
-  return json({ authenticated: true, user });
-}
